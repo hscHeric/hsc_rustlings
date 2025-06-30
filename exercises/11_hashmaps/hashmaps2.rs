c804fec9604cc129148db5a48d4986bff0bbaeb7
Heric da Silva Cruz <hericsilvaho@gmail.com>
2025-06-30 14:12:17 -0300

    atualizando

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,12 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        //
+        if let Some(counter) = basket.get_mut(&fruit) {
+            *counter += 1;
+        } else {
+            basket.insert(fruit, 1);
+        }
     }
 }
 
